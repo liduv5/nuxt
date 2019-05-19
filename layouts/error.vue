50bb8a1469993505c4fc38266352e4854b5d5e7f
liduv5 <570577701@qq.com>
2019-05-19 23:57:20 +0800

    404

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">页面不存在</h1>
+    <h1 v-else>您没有访问权限</h1>
+    <nuxt-link to="/">首 页</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
