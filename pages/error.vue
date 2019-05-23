73db682aa1b0ebf2f03122094a7c8c784ea9ac36
liduv5 <570577701@qq.com>
2019-05-23 22:44:52 +0800

    错误拦截

--- pages/error.vue
+++ pages/error.vue
@@ -1,7 +1,6 @@
 <template>
    <div>
-       <h1>404错误</h1>
+       <h1>您暂时没有权限访问此地！</h1>
        <h2>请去你该去的地方！</h2>
-       {{$route.query.status}}
    </div>
 </template>
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,8 @@
 export default function ({ $axios, redirect }) {
     $axios.onError(err => {
         const code = parseInt(err.response && err.response.status)
-        if (code === 456) {
-            redirect('/404?status=456')
+        if (code === 401) {
+            redirect('/error')
         }
     })
 }
